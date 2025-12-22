7465c7ca3b4c8edb5111c07b8d27101c41a5bec7
henriqk0 <171154572+henriqk0@users.noreply.github.com>
2025-12-22 17:22:02 -0300

    build(blade): removing front-end laravel code and renaming the full-stack folder to backend

--- full-stack/resources/js/utils/loading.js
+++
@@ -1,9 +0,0 @@
-const loading = document.getElementById('loading');
-
-export function showLoading() {
-    loading.classList.remove('hidden');
-}
-
-export function hideLoading() {
-    loading.classList.add('hidden');
-}
